480e3b553ec7a659de54757d97a9a589a5e15979
shendesuchit <shendesuchit@gmail.com>
2019-10-15 16:22:43 +0530

    Add files via upload
    Spring cloud Basics == Microservices

---
+++ spring-eureka-client-student-service/src/main/java/com/saama/studentservice/controller/StudentServiceController.java
@@ -0,0 +1,63 @@
+package com.saama.studentservice.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.saama.studentservice.domain.Student;
+
+@RestController
+public class StudentServiceController {
+
+	private static Map<String, List<Student>> schooDB = new HashMap<String, List<Student>>();
+
+	static {
+		schooDB = new HashMap<String, List<Student>>();
+
+		List<Student> lst = new ArrayList<Student>();
+		Student std = new Student("Sajal", "Class IV");
+		lst.add(std);
+		std = new Student("Lokesh", "Class V");
+		lst.add(std);
+
+		schooDB.put("abcschool", lst);
+
+		lst = new ArrayList<Student>();
+		std = new Student("Kajal", "Class III");
+		lst.add(std);
+		std = new Student("Sukesh", "Class VI");
+		lst.add(std);
+
+		schooDB.put("xyzschool", lst);
+
+	}
+
+	@GetMapping("/getSchoolList")
+	public List<String> getSchoolList() {
+
+		ArrayList<String> strlist = new ArrayList<String>();
+		schooDB.forEach((k, v) -> strlist.add(k));
+
+		return strlist;
+
+	}
+	
+	
+	@GetMapping(value = "/getStudentDetailsForSchool/{schoolname}")
+	public List<Student> getStudents(@PathVariable String schoolname) {
+		System.out.println("Getting Student details for " + schoolname);
+
+		List<Student> studentList = schooDB.get(schoolname);
+		if (studentList == null) {
+			studentList = new ArrayList<Student>();
+			Student std = new Student("Not Found", "N/A");
+			studentList.add(std);
+		}
+		return studentList;
+	}
+}
